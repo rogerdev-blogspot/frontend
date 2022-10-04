cc83e1fc5b711b7e8a776c7c2b5e8583a1044d93
ademawan <ademawan1210@gmail.com>
2022-10-04 23:11:10 +0700

    add redirect url

--- delivery/controllers/auth/auth.go
+++ delivery/controllers/auth/auth.go
@@ -140,7 +140,7 @@ func (ac *AuthController) Register() echo.HandlerFunc {
 		// return c.Redirect(200, "/login")
 		message += " 5 success"
 
-		return c.JSON(http.StatusOK, message)
+		return c.Redirect(200, "/login")
 
 	}
 }
